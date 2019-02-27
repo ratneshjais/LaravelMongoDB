498622b3dcd2c4ae71e224505919459d0ba77e82
Ratnesh <ratnesh@stelcore.in>
2019-02-27 15:41:42 +0530

    Laravel + MongoDB

---
+++ laravelmongodb/app/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+
+
+class User extends Eloquent implements Authenticatable
+{
+//    use Notifiable;
+    use AuthenticableTrait;
+
+    protected $connection = 'mongodb';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
